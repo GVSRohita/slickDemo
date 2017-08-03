be62948758d16abc26e4a4572c92535dc7737d09
GVSRohita <gvsrohita@gmail.com>
2017-08-03 17:30:20 +0530

    Add files via upload
    added comments

--- js/myBuilder.js
+++ js/myBuilder.js
@@ -6,27 +6,27 @@
 $(document).ready(function () {
     $('.puppy').slick({
         lazyLoad: 'ondemand',
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        centerMode: true,
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        adaptiveHeight: true,
-        responsive: [
+        slidesToShow: 3,// the desktop to show the number of images
+        slidesToScroll: 1,// the images to be scrolled
+        centerMode: true,// enables centered view
+        dots: true,// for the dots to appear
+        infinite: true,// for infinite loop sliding
+        autoplay: true, // automatic movement of the images is enabled
+        autoplaySpeed: 2000,// the time with which the images slide is given in milliseconds
+        adaptiveHeight: true,// to adapt the size of the images
+        responsive: [// to make the page responsive
             {
-                breakpoint: 768,
+                breakpoint: 768,// first break point at 768 px.(Reduce the size of the screen to see the magic!)
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 1,
                     centreMode: true
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 480,// second break point at 480 px.(Reduce the size of the screen to see the magic!)
                 settings: {
-                    fade: true,
+                    fade: true,// to enable fade effect on the page when used in mobile screens
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     centreMode: true
